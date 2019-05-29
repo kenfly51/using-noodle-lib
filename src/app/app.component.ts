e0d9e01a4a5ce27ff2c0dcb441c8efd4ec86444c
Ngan Thanh Nguyen <nganthanhnguyen@ngan.local>
2019-05-30 01:28:16 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { SideBarContainerComponent } from 'noodle-lib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'using-noodle-lib';
+export class AppComponent implements AfterViewInit {
+  @ViewChild(SideBarContainerComponent) sideBar: SideBarContainerComponent
+
+  loggedIn: boolean = false;
+
+  submitted: boolean = false;
+
+  loggingIn: boolean = false;
+
+  toggleSidebar() {
+    this.sideBar.toggleSidebar();
+  }
+
+  onLogin($event) {
+    this.submitted = true;
+    this.loggingIn = true;
+
+    // call login api from server
+    console.log($event)
+
+    this.loggedIn = true;
+  }
+
+  constructor(){
+  }
+
+  ngAfterViewInit() {
+  }
 }
